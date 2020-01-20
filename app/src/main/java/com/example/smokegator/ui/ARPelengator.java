ac5c188faf989ffc3d539e995c5bf02429383142
rn3aoc <sukhov.sergey@gmail.com>
2020-01-20 14:05:21 +0300

    AR Pelengator

---
+++ app/src/main/java/com/example/smokegator/ui/ARPelengator.java
@@ -0,0 +1,16 @@
+package com.example.smokegator.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smokegator.R;
+
+public class ARPelengator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arpelengator);
+    }
+}
